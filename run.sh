451cab9bc94af6240dc4b775bdb9138367907124
罗昱成 <695737315@qq.com>
2019-12-26 17:22:42 +0800

    fuck sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # baseline model
 python3 train.py \
 --train_data data_lmdb_release/training --valid_data data_lmdb_release/validation \
---select_data /--batch_ratio 1.0 \
---Transformation None --FeatureExtraction OCResNet --SequenceModeling BiLSTM --Prediction Attn
+--select_data / --batch_ratio 1.0 \
+--Transformation None --FeatureEx
