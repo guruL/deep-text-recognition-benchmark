b077b33312bf0dcf23de3505540b1f95c536080a
罗昱成 <695737315@qq.com>
2019-12-28 11:14:35 +0800

    run.sh error

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 # baseline model
 python3 train.py \
--- manualSeed 2222 \
+--manualSeed 2222 \
 --train_data data_lmdb_release/training --valid_data data_lmdb_release/validation \
 --select_data / --batch_ratio 1.0 \
 --Transformation None --FeatureExtraction OCResNet --SequenceModeling BiLSTM --Prediction Attn
